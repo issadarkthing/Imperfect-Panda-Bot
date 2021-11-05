9ab000d66cfb81b4216a73fd99b3f1720ac82a2e
raziman <raziman.terra@gmail.com>
2021-11-05 18:40:06 +0800

    add Party command

--- src/commands/Party.ts
+++ src/commands/Party.ts
@@ -1,13 +1,24 @@
 import { Command } from "@jiman24/commandment";
-import { Message } from "discord.js";
+import { Message, MessageEmbed } from "discord.js";
+import { Player } from "../structure/Player";
+import { toNList } from "../utils";
 
 
 
 export default class extends Command {
   name = "party";
+  aliases = ["p"];
 
   async exec(msg: Message) {
 
-    msg.channel.send("hello world");
+    const player = new Player(msg.author);
+    const pandas = toNList(player.pandas.map(x => x.name)) || "none";
+
+    const embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Party")
+      .setDescription(pandas);
+
+    msg.channel.send({ embeds: [embed] });
   }
 }
