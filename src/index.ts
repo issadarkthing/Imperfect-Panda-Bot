57b30cdd9f3e609538feb8ba7ea00926f653cb94
raziman <raziman.terra@gmail.com>
2021-11-05 11:32:42 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { Client } from "./structure/Client";
+import path from "path";
+import { config } from "dotenv";
+
+config();
+
+const client = new Client({ 
+  intents: [
+    "GUILDS", 
+    "GUILD_MESSAGES",
+    "DIRECT_MESSAGES",
+    "GUILD_MESSAGE_REACTIONS",
+    "GUILD_MEMBERS",
+  ],
+  partials: [
+    "CHANNEL",
+    "GUILD_MEMBER",
+    "REACTION",
+  ]
+});
+
+const commandManager = client.commandManager;
+
+commandManager.verbose = true;
+commandManager.registerCommands(path.resolve(__dirname, "./commands"));
+
+commandManager.registerCommandNotFoundHandler((msg, cmdName) => {
+  msg.channel.send(`Cannot find command "${cmdName}"`);
+})
+
+commandManager.registerCommandOnThrottleHandler((msg, cmd, timeLeft) => {
+  const time = (timeLeft / 1000).toFixed(2);
+  msg.channel.send(`You cannot run ${cmd.name} command after ${time} s`);
+})
+
+client.on("ready", () => console.log(client.user?.username, "is ready!"))
+client.on("messageCreate", msg => commandManager.handleMessage(msg));
+
+client.login(process.env.BOT_TOKEN);
---
+++ src/structure/Client.ts
@@ -0,0 +1,10 @@
+import { CommandManager } from "@jiman24/commandment";
+import { Client as DiscordClient } from "discord.js";
+import Enmap from "enmap";
+
+export class Client extends DiscordClient {
+  players = new Enmap("Player");
+  commandManager = new CommandManager(process.env.PREFIX || "p!");
+  adminID = "117796576067321865";
+  devID = "264010327023288323";
+}
