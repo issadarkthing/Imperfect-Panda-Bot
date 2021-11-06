a487025477ecc9ff43df8e89a61b7e7ccde8d414
raziman <raziman.terra@gmail.com>
2021-11-06 12:11:43 +0800

    add Nickname command

---
+++ src/commands/Nickname.ts
@@ -0,0 +1,33 @@
+import { Command } from "@jiman24/commandment";
+import { Message } from "discord.js";
+import { Player } from "../structure/Player";
+import { bold } from "../utils";
+
+
+export default class extends Command {
+  name = "nickname";
+
+  async exec(msg: Message, args: string[]) {
+
+    const [id, nickname] = args;
+    if (!id) {
+      throw new Error("please provide id or nickname");
+    } else if (!nickname) {
+      throw new Error("please provide a new nickname");
+    }
+
+    const player = new Player(msg.author);
+    const panda = player.getPanda(id);
+
+    if (!panda) {
+      throw new Error("cannot find panda");
+    }
+
+    const oldName = panda.name;
+
+    panda.nickname = nickname;
+    player.save();
+
+    msg.channel.send(`Successfully change ${bold(oldName)} to ${bold(panda.name)}!`);
+  }
+}
